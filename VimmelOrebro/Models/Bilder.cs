c2e10f067372a9fb79b0af9904b6c8cafa02e34b
MånsBergman <mans.bergman@gmail.com>
2016-04-01 10:14:08 +0200

    model

---
+++ VimmelOrebro/Models/Bilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VimmelOrebro.Models
+{
+    public class Bilder
+    {
+        [Key]
+        public int BID { get; set; }
+
+        public string bildPath { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
